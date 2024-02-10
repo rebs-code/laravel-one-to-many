7ea765dca9f56305584978055064b958d5936cb6
rebs-code <jack.rebussi@gmail.com>
2024-02-10 16:45:59 +0100

    create: seeder for types seeder

--- database/migrations/2024_02_10_153721_create_types_table.php
+++ database/migrations/2024_02_10_153721_create_types_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('type', 30)->unique();
+            $table->string('slug');
             $table->timestamps();
         });
     }
