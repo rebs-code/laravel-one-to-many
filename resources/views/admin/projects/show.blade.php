ba8dd896e751a20fcb1b2f72aff18c962a271528
rebs-code <jack.rebussi@gmail.com>
2024-02-11 16:11:48 +0100

    feat: show type @if exists

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,6 +7,13 @@
                 <h1>{{ $project->name }}</h1>
                 <p>{{ $project->description }}</p>
                 <p>{{ $project->tech_stack }}</p>
+                @if ($project->type->name)
+                    <p>{{ $project->type->name }}</p>
+                @endif
+                {{-- @if ($project->image)
+                    <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->name }}" class="img-fluid"
+                        style="max-width: 300px;">
+                @endif --}}
                 <p>{{ $project->repo_link }}</p>
                 <p>{{ $project->slug }}</p>
                 <a href="{{ route('admin.projects.index') }}" role="button" class="btn btn-primary">Back</a>
